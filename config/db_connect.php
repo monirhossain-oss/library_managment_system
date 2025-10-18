e7853ffff4e732ec8ece5c39d7dd9232a5d04482
Monir Hossain <mdmonirhos571@gmail.com>
2025-10-19 01:56:52 +0600

    Clean up db_connect.php by removing comments
    Removed commented optional success message from db_connect.php.

--- config/db_connect.php
+++ config/db_connect.php
@@ -4,17 +4,15 @@ define('BASE_URL', 'http://localhost/projects/library_app/');
 
 
 $servername = "localhost";
-$username = "root"; // Default XAMPP username
-$password = "";     // Default XAMPP password is empty
+$username = "root"; 
+$password = ""; 
 $dbname = "library_pro";
 
-// Create connection
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Optional success message (for testing)
-// echo "Database connected successfully!";
